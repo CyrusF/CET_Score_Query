b3b1ca91c2170530ac87b7c2dee48c6c69efac6c
Cyrus Fang <fcs98@sina.com>
2017-02-25 00:16:52 +0800

    Update cet2.0.py

--- cet2.0.py
+++ cet2.0.py
@@ -4,29 +4,45 @@ import requests
 import os,sys
 import xlrd,xlwt
 
+errorlist=["0","Referer Error","Query Error","Encoding Error","傻逼Python又出错了","傻逼GBK编码又有BUG了","辣鸡成电网又断了"]
+
 def query(i,zkzh,xm,jb):
     u=requests.session()
     u.cookies.clear()
-    url = "http://cet.99sushe.com/getscore"
-    headers = {"Referer": "http://cet.99sushe.com"}
-    postdata = {"id": zkzh, "name": xm.encode('GBK')}
-    r = u.post(url, headers=headers, data=postdata).text.strip()
-    if (r==""):
-        return 0
+    try:
+        url = "http://cet.99sushe.com/getscore"
+        headers = {"Referer": "http://cet.99sushe.com"}
+        postdata = {"id": zkzh, "name": xm[:2].encode('GBK')}
+    except:
+        return 5
     else:
-        dict = r.split(',')
-        if (dict[9].partition('--')[1]=='--'):
-            dict[9]='\\'
-        if (dict[10].partition('--')[1]=='--'):
-            dict[10]='\\'
-        ressheet.write(i, 0, xm)
-        ressheet.write(i, 1, jb)
-        ressheet.write(i, 2, dict[5])
-        ressheet.write(i, 3, dict[2])
-        ressheet.write(i, 4, dict[3])
-        ressheet.write(i, 5, dict[4])
-        ressheet.write(i, 6, dict[10])
-    return 1
+        pass
+    try:
+        r = u.post(url, headers=headers, data=postdata).text.strip()
+    except:
+        return 6
+    else:
+        pass
+    if (len(r)==1):
+        return int(r)
+    else:
+        try:
+            dictr = r.split(',')
+            if (dictr[9].partition('--')[1]=='--'):
+                dictr[9]='\\'
+            if (dictr[10].partition('--')[1]=='--'):
+                dictr[10]='\\'
+            ressheet.write((i-1)%5000+1, 0, xm)
+            ressheet.write((i-1)%5000+1, 1, jb)
+            ressheet.write((i-1)%5000+1, 2, dictr[5])
+            ressheet.write((i-1)%5000+1, 3, dictr[2])
+            ressheet.write((i-1)%5000+1, 4, dictr[3])
+            ressheet.write((i-1)%5000+1, 5, dictr[4])
+            ressheet.write((i-1)%5000+1, 6, dictr[10])
+        except:
+            return 4
+        else:
+            return 0
 
 print("准备查询")
 os.system("chcp 437")
@@ -49,29 +65,35 @@ ressheet.write(i, 6, '口语')
 ok=0;
 error=0;
 
-for i in range(1,nrows):
-    zkzh=table.cell(i,5).value
-    xm=table.cell(i,6).value
-    jb=table.cell(i,1).value
-    if (query(i,zkzh,xm,jb)==1):
-        print("[%05d] %s - OK!" % (i,zkzh))
+for i in range(9999,nrows+1):
+    zkzh=table.cell(i,5).value.strip()
+    xm=table.cell(i,6).value.strip()
+    jb=table.cell(i,1).value.strip()
+    k=query(i,zkzh,xm,jb)
+    if (k==0):
+        print("[%05d] %s - OK!" % (i,xm))
         ok+=1
     else:
-        print("[%05d] %s - ERROR!" % (i,zkzh))
-        if (query(i,zkzh,xm,jb)==1):
-            print("Try Again [%03d] %s - OK!" % (i,zkzh))
+        print("[%05d] %s - %s!" % (i,xm,errorlist[k]))
+        k=query(i,zkzh,xm,jb)
+        if (k==0):
+            print("Try Again [%03d] %s - OK!" % (i,xm))
             ok+=1
         else:
-            print("Try Again [%05d] %s - ERROR!" % (i,zkzh))
-            if (query(i,zkzh,xm,jb)==1):
-                print("Try Again [%05d] %s - OK!" % (i,zkzh))
+            print("Try Again [%05d] %s - %s!" % (i,xm,errorlist[k]))
+            k=query(i,zkzh,xm,jb)
+            if (k==0):
+                print("Try Again [%05d] %s - OK!" % (i,xm))
                 ok+=1
             else:
-                print("Try Again [%05d] %s - ERROR!" % (i,zkzh))
+                print("Try Again [%05d] %s - %s!" % (i,xm,errorlist[k]))
                 error+=1
-    i+=1
+    if (i%5000==0):
+        resworkbook.save('Result'+str(int(i/5000))+'.xls')
+        print('Saved Workbook %d'%(i/5000))
 
-print("查询完毕，共计 %d 条结果"%(i-1))
+print("查询完毕，共计 %d 条结果"%(i))
 print("其中 %d 条结果查询失败"%(error))
-resworkbook.save('Result.xls')
+if (i%5000!=0):
+    resworkbook.save('Result'+str(int(i/5000+1))+'.xls')
 os.system("pause")
